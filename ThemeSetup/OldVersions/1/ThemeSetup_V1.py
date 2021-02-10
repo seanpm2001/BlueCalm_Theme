28bd2e009fbcdc757136f2ba0cdb1a4ef040310d
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-02-10 15:06:43 -0800

    ThemeSetup_V1.py
    An archive of the first version of theme customization script for BlueCalm.

---
+++ ThemeSetup/OldVersions/1/ThemeSetup_V1.py
@@ -0,0 +1,22 @@
+# Start of script
+"""
+Info
+This is the theme customization script for BlueCalm. It is not the script for the actual theme. This script alone will not let you enable the theme.
+"""
+# Main
+print ("BlueCalm theme customization")
+print ("Theme mode:\nAnimated and advanced\nGradient\nSolid color")
+print ("Blue theme type:\nCalm (Navy Blue) - 8 color variations\nSky blue - ? color variations\nAqua marina - ? color variations\nOther/custom - ? color variations")
+print ("Extra features:\nEnable 24 hour time\nShuffle through colors\nEnable clouds\nEnable transparency\nEnable airplanes\nEnable bubbles\nEnable fish\nCustomize fish")
+# Divider
+print ("Customization is currently unavailable")
+noMore = input("Press [ENTER] key to quit")
+print ("This window should now be closed. If the process is still running, try closing the window with the close button. If this doesn't work, end the process/task with a task/process manager")
+break
+'''
+File info
+File type: Python source file (*.py)
+File version: 1 (Wednesday, February 10th 2021 at 3:06 pm)
+Line count (including blank lines and compiler line): 23
+'''
+# End of script
